3a22378f4476231c78683b42b0065292fa2f9081
Miles Marsh <msmarsh@ucsc.edu>
2023-10-15 11:23:00 -0700

    fixing lint errors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Miles Marsh";
+const gameName = "Sticker Sketchpad";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = <HTMLCanvasElement>document.getElementById("canvas");
+const ctx = canvas!.getContext("2d");
+
+ctx!.fillStyle = "green";
+ctx!.fillRect(10, 10, 150, 100);
