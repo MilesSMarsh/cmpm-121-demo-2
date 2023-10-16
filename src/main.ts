faf12f5aaef9a3647f9412a83cbf48b98b82d770
Miles Marsh <msmarsh@ucsc.edu>
2023-10-16 08:08:49 -0700

    added drawing and clear button

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,61 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-// const ctx = canvas!.getContext("2d");
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas!.getContext("2d");
+
+//vvvvvvvvvvvv Code from https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event vvvvvvvvv
+// When true, moving the mouse draws on the canvas
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+// event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+canvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(ctx!, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+window.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    drawLine(ctx!, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(
+  context: CanvasRenderingContext2D,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+const clearButton = document.getElementById("clear");
+
+clearButton?.addEventListener("click", function handleClick(event) {
+  console.log(event);
+  ctx!.clearRect(0, 0, canvas.width, canvas.height);
+});
